538f9d8a336057d01f5b56b57ec97e66b943e7cf
Mohammedtufail130 <tufailmuhammed031@gmail.com>
2025-04-15 00:08:19 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 require('./models/dbConnection');
 const authRouter = require('./routes/authRouter');
 const cors = require('cors');
