80850adce1750cfb212f0db8050a7a472dec50ee
Mohammedtufail130 <tufailmuhammed031@gmail.com>
2025-04-16 16:22:47 +0530

    frontend api cng

--- frontend/src/GoogleLogin.jsx
+++ frontend/src/GoogleLogin.jsx
@@ -1,5 +1,5 @@
 import { useGoogleLogin } from "@react-oauth/google";
-import { googleAuth } from "./server";
+import { googleAuth } from "./api";
 import { useNavigate } from "react-router-dom";
 import './GoogleLogin.css';
 
