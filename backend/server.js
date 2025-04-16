78d52ff8c4dc34b0d23c6802b09c82b93b31c4be
Mohammedtufail130 <tufailmuhammed031@gmail.com>
2025-04-17 00:18:00 +0530

    port added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 'https://googlesignin-two.vercel.app';
 require('./models/dbConnection');
 const authRouter = require('./routes/authRouter');
 const cors = require('cors');
