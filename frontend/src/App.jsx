01aeb8f363b189fbd3ce5ac58f59828b9bd4a63e
Mohammedtufail130 <tufailmuhammed031@gmail.com>
2025-04-18 22:01:54 +0530

    app.jsx mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-import {BrowserRouter,Route, Routes, Navigate} from 'react-router-dom';
+import { useState } from 'react';
+import './App.css';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 import GoogleLogin from './GoogleLogin';
 import Dashboard from './Dashboard';
 import PageNotFound from './PageNotFound';
-import {GoogleOAuthProvider} from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import RefrshHandler from './RefreshHandler';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const GoogleAuthWrapper = ()=>{
-      return (
-        <GoogleOAuthProvider clientId='946447966764-4v1mhpqud8t08v4224iog3c2se8d5tif.apps.googleusercontent.com'>
-          <GoogleLogin></GoogleLogin>
-        </GoogleOAuthProvider>
-      )
-    }
-    const PrivateRoute = ({ element }) => {
-      return isAuthenticated ? element : <Navigate to="/login" />;
-    }
- 
+  const GoogleAuthWrapper = () => (
+    <GoogleOAuthProvider clientId="946447966764-4v1mhpqud8t08v4224iog3c2se8d5tif.apps.googleusercontent.com">
+      <GoogleLogin />
+    </GoogleOAuthProvider>
+  );
+
+  const PrivateRoute = ({ element }) =>
+    isAuthenticated ? element : <Navigate to="/login" />;
+
   return (
-    <BrowserRouter>
-     <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
-        <Routes>
-          <Route path="/login" element={<GoogleAuthWrapper/>}/>
-          <Route path="/" element={<Navigate to="/login"/>}/>
-          <Route path="/dashboard" element={<PrivateRoute element={<Dashboard/>}/>}/>
-          <Route path="*" element={<PageNotFound/>}/>
-        </Routes>
-        </BrowserRouter>
-  )
+    <HashRouter>
+      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
+      <Routes>
+        <Route path="/login" element={<GoogleAuthWrapper />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute element={<Dashboard />} />}
+        />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
