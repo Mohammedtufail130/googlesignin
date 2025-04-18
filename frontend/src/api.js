833621aa27c661b6a52ab1eb6563b118a36f7bd3
Mohammedtufail130 <tufailmuhammed031@gmail.com>
2025-04-18 20:02:11 +0530

    baseURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://googlesignin-2xls.onrender.com'
+    baseURL: 'https://googlesignin-2xls.onrender.com/auth'
 });
 
 export const googleAuth = (code) => api.get(`/google?code=${code}`);
